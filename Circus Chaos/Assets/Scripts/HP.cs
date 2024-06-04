77a98fa26e7ee92d60f745dba187ef6f5e5e2efe
MosquitoEstudante234 <mosquitoestudante34234@gmail.com>
2024-06-04 11:23:46 -0300

    Feedback visual do HP

--- Circus Chaos/Assets/Scripts/HP.cs
+++ Circus Chaos/Assets/Scripts/HP.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class HP : MonoBehaviour
 {
+    public GameObject Heart1, Heart2, Heart3;
     public static int Hearts = 3;
     void Start()
     {
@@ -15,6 +16,30 @@ public class HP : MonoBehaviour
         if (Hearts <= 0)
         {
             SceneManager.LoadScene("cena1");
+        }
+        if (Hearts == 0)
+        {
+            Heart1.SetActive(false);
+            Heart2.SetActive(false);
+            Heart3.SetActive(false);
+        }
+        if (Hearts == 1)
+        {
+            Heart1.SetActive(true);
+            Heart2.SetActive(false);
+            Heart3.SetActive(false);
+        }
+        if (Hearts == 2)
+        {
+            Heart1.SetActive(true);
+            Heart2.SetActive(true);
+            Heart3.SetActive(false);
+        }
+        if (Hearts == 3)
+        {
+            Heart1.SetActive(true);
+            Heart2.SetActive(true);
+            Heart3.SetActive(true);
         }
 
     }
