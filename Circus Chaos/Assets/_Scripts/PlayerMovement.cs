d34ca7b247a7374aed9c498c9caf42905d494a4d
MosquitoEstudante234 <mosquitoestudante34234@gmail.com>
2024-06-18 16:39:16 -0300

    Re-design do jogo começado
    Sistema de dialogo e joysticks implementados

---
+++ Circus Chaos/Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D body;
+    public FixedJoystick joystick;
+
+    public float speed = 5;
+
+    public static float direction;
+
+    public Animator animator;
+
+    public static Transform posPlayer;
+    public SpriteRenderer spriteRenderer;
+    public static PlayerMovement Instance;
+
+    //public Transform weapon;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    void Start()
+    {
+        posPlayer = transform;
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+        if (joystick.Horizontal != 0)
+        {
+            direction = joystick.Horizontal;
+        }
+
+        if (joystick.Horizontal < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        if (joystick.Horizontal > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+
+        animator.SetBool("Walking", joystick.Horizontal != 0 || joystick.Vertical != 0);
+        if (gameObject.transform.childCount > 1)
+        {
+            foreach (Transform Child in gameObject.transform)
+            {
+                Destroy(Child.gameObject);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        body.velocity = new Vector2(joystick.Horizontal * speed, joystick.Vertical * speed);
+    }
+}
