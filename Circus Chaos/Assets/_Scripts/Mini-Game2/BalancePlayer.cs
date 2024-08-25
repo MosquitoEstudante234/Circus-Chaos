d8fdb406a60d196d8943ada004889fb005e1c759
astronomo15 <garoncunim@gmail.com>
2024-08-25 19:41:26 -0300

    mecanica corda bamba feita

---
+++ Circus Chaos/Assets/_Scripts/Mini-Game2/BalancePlayer.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BalancePlayer : MonoBehaviour
+{
+    public float rotationSpeed = 10f;
+    public float moveSpeed = 0.001f; // Velocidade de movimento no eixo X
+    public Button increaseButton;
+    public Button decreaseButton;
+
+    private float minXPosition = 39.70f; // Limite mínimo do eixo X
+    private float maxXPosition = 40.30f;  // Limite máximo do eixo X
+
+    private void Start()
+    {
+        // Verifica a posição inicial para garantir que está dentro dos limites
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, minXPosition, maxXPosition),
+            transform.position.y,
+            transform.position.z
+        );
+
+        increaseButton.onClick.AddListener(IncreaseRotation);
+        decreaseButton.onClick.AddListener(DecreaseRotation);
+    }
+
+    void Update()
+    {
+        float zRotation = transform.eulerAngles.z;
+
+        // Converte a rotação de 360 para -180 a 180 graus
+        if (zRotation > 180)
+        {
+            zRotation -= 360;
+        }
+
+        // Controle da rotação
+        if (zRotation > -1 && zRotation < 1)
+        {
+            zRotation++;
+        }
+
+        // Movimento no eixo X baseado na rotação
+        float moveAmount = moveSpeed * Time.deltaTime;
+        moveAmount = Mathf.Clamp(moveAmount, -0.005f, 0.005f); // Limita o movimento para evitar velocidades extremas
+
+        if (zRotation >= 0)
+        {
+            transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+            transform.position += new Vector3(-moveAmount, 0, 0); // Move para a esquerda
+        }
+        else if (zRotation <= -1)
+        {
+            transform.Rotate(0, 0, -rotationSpeed * Time.deltaTime);
+            transform.position += new Vector3(moveAmount, 0, 0); // Move para a direita
+        }
+
+        // Restringe a posição do eixo X para estar entre -0.25 e 0.25
+        float clampedX = Mathf.Clamp(transform.position.x, minXPosition, maxXPosition);
+        transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
+
+
+
+        // Condições de queda
+        if (zRotation >= 50)
+        {
+            Debug.Log("Caiu2");
+        }
+        if (zRotation <= -50)
+        {
+            Debug.Log("Caiu1");
+        }
+    }
+
+    public void IncreaseRotation()
+    {
+        transform.Rotate(0, 0, 5f);
+    }
+
+    public void DecreaseRotation()
+    {
+        transform.Rotate(0, 0, -5f);
+    }
+}
