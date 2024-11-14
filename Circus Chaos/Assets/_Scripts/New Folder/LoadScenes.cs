ab6927942e8e32eb384e71026872a7b551635642
Behannel <mosquitoestudante34234@gmail.com>
2024-11-14 08:46:15 -0300

    Começando a transferir os minigames pra cenas diferentes

---
+++ Circus Chaos/Assets/_Scripts/New Folder/LoadScenes.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadScenes : MonoBehaviour
+{
+    public void CenaUm()
+    {
+
+    }
+    public void CenaDois()
+    {
+
+    }
+    public void CenaTres()
+    {
+
+    }
+    public void CenaQuatro()
+    {
+
+    }
+    public void CenaCinco()
+    {
+
+    }
+    public void CenaSeis()
+    {
+
+    }
+}
