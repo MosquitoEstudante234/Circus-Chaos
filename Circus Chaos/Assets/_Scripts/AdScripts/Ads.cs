d52c8b0f5b3f8eae956d97a7097bf9869e71b04b
Behannel <mosquitoestudante34234@gmail.com>
2024-11-26 13:26:20 -0300

    Códigos para anúncios adicionados

---
+++ Circus Chaos/Assets/_Scripts/AdScripts/Ads.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class Ads : MonoBehaviour, IUnityAdsInitializationListener
+{
+    [SerializeField]
+    bool _testMode = true;
+    IUnityAdsShowListener adsListener;
+    public static bool isShowing;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Advertisement.Initialize("SuaIDAqui", _testMode, this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnInitializationComplete()
+    {
+        print("Inicializou");
+    }
+
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+    {
+        print("Deu ruim");
+    }
+}
